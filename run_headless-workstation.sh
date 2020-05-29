4792d94be8510fcad137c5877567ca721812e3e4
mikaelmoutakis <mikaelmoutakis@gmail.com>
2020-05-29 08:23:37 +0200

    Cache invalidated after 1 day

--- run_headless-workstation.sh
+++ run_headless-workstation.sh
@@ -6,6 +6,6 @@ cd ~/apotekstider
 --export-cache='../exported_pages' \
 --headless \
 --suppress-errors \
---exec='./misc/send_output_files_with_email.py {}' \
+--exec='./misc/send_output_files_with_email.py --export-cache={} {}' \
 ALLA && shutdown
 
