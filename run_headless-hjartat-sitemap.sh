1d55b00c2d861a4f4d5770a8e38bf6294d215b93
mikaelmoutakis <mikaelmoutakis@gmail.com>
2020-06-03 10:47:45 +0200

    added function for saving sitemaps

---
+++ run_headless-hjartat-sitemap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+cd ~/apotekstider
+./skrapa \
+--profile='../firefox-profile' \
+--output='../output' \
+--cache='../cache' \
+--headless \
+--suppress-errors \
+--exec='./misc/send_output_files_with_email.py {}' \
+hjartat
+
